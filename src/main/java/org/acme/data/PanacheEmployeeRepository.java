c3ed927d386b31b8187bc25a84fb564978d14060
Eduardo Vinicius Kempf <edukempf@gmail.com>
2022-12-06 14:50:51 -0300

    implementado repositorio para panache para criar funcionario

---
+++ src/main/java/org/acme/data/PanacheEmployeeRepository.java
@@ -0,0 +1,31 @@
+package org.acme.data;
+
+import io.quarkus.panache.common.Parameters;
+import org.acme.domain.employee.CreateEmployeeDTO;
+import org.acme.domain.employee.Employee;
+import org.acme.infra.PanacheEmployee;
+
+import javax.transaction.Transactional;
+
+public class PanacheEmployeeRepository implements EmployeeRepository {
+    private final String HQL_SELECT_BY_EMAIL =
+    "SELECT employee FROM MySqlEmployee as employee WHERE employee.email = :email";
+    @Override
+    @Transactional
+    public Employee findByEmail(String email) {
+        PanacheEmployee employee =
+                PanacheEmployee.find(HQL_SELECT_BY_EMAIL, Parameters.with("email", email)).firstResult();
+        return employee == null ? null : employee.toEmployee();
+    }
+
+    @Override
+    @Transactional
+    public Employee create(CreateEmployeeDTO employee) {
+        PanacheEmployee panacheEmployee = new PanacheEmployee();
+        panacheEmployee.email = employee.email;
+        panacheEmployee.password = employee.password;
+        panacheEmployee.name = employee.name;
+        panacheEmployee.persist();
+        return panacheEmployee.toEmployee();
+    }
+}
---
+++ src/main/java/org/acme/infra/PanacheEmployee.java
@@ -0,0 +1,29 @@
+package org.acme.infra;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.acme.domain.employee.Employee;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class PanacheEmployee extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long id;
+    @Column(name = "email")
+    public String email;
+    @Column(name = "password")
+    public String password;
+    @Column(name = "name")
+    public String name;
+    public Employee toEmployee() {
+        Employee employee = new Employee();
+        employee.email = this.email;
+        employee.password = this.password;
+        employee.name = this.name;
+        employee.id = this.id;
+        return employee;
+    }
+}
