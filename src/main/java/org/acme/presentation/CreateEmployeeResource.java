3bae43db92e1c78a2b9d59c95d2e8a38fad0e6a4
Eduardo Vinicius Kempf <edukempf@gmail.com>
2022-12-06 15:44:04 -0300

    implementada rota de criacao de funcionarios

---
+++ src/main/java/org/acme/presentation/CreateEmployeeResource.java
@@ -0,0 +1,25 @@
+package org.acme.presentation;
+
+import org.acme.domain.employee.CreateEmployeeDTO;
+import org.acme.domain.employee.Employee;
+import org.acme.main.CreateEmployeeService;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("employee")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CreateEmployeeResource {
+    @Inject
+    CreateEmployeeService createEmployeeService;
+    @POST
+    public Employee handle(@RequestBody CreateEmployeeDTO createEmployeeDTO) {
+        return createEmployeeService.handle(createEmployeeDTO);
+    }
+}
